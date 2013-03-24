88be17359d06fb034afc9582f0c82ea0f6f4a328
heat3203@gmail.com <heat3203@gmail.com@9925db87-bd3a-aace-cc7a-927a7ff157de>
2013-03-24 19:17:28 +0000

    model

---
+++ trunk/eco.professor/src/main/java/org/usac/eco/professor/model/ECOFrame.java
@@ -0,0 +1,153 @@
+/*
+ *  Copyright (C) 2013 ronyHeat3203
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.usac.eco.professor.model;
+
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author ronyHeat3203
+ */
+public class ECOFrame extends MainFrame{
+
+     private JMenuBar jmbMenuBar;
+
+    private JMenu jmFile, jmHelp;
+
+    private JMenuItem jmiLogOut, jmiExit, jmiAbout;
+
+    private JLabel userName, user;
+
+    private ActionListener onExit, onLogOut, onAbout;
+
+    public ECOFrame ()
+    {
+        super();
+
+        this.jmbMenuBar = new JMenuBar();
+        this.jmbMenuBar.setVisible(true);
+
+        this.jmFile = new JMenu("Archivo");
+        this.jmFile.setVisible(true);
+        this.jmHelp = new JMenu("Ayuda");
+        this.jmHelp.setVisible(true);
+
+        this.jmiLogOut = new JMenuItem("Cerrar Sesión");
+        this.jmiExit   = new JMenuItem("Salir");
+        this.jmiAbout  = new JMenuItem("Acerca De");
+
+        this.jmbMenuBar.add(this.jmFile);
+        this.jmbMenuBar.add(this.jmHelp);
+
+        this.jmFile.add(this.jmiLogOut);
+        this.jmFile.add(this.jmiExit);
+
+        this.jmHelp.add(this.jmiAbout);
+
+        this.setJMenuBar(this.jmbMenuBar);
+
+        this.user = new JLabel();
+        this.userName = new JLabel();
+
+        this.jmiExit.addActionListener(this.onExit);
+        this.jmiLogOut.addActionListener(this.onLogOut);
+        this.jmiAbout.addActionListener(this.onAbout);
+
+    }
+
+    public JMenu getJmFile() {
+        return jmFile;
+    }
+
+    public void setJmFile(JMenu jmFile)
+    {
+        this.jmFile = jmFile;
+    }
+
+    public JMenu getJmHelp()
+    {
+        return jmHelp;
+    }
+
+    public void setJmHelp(JMenu jmHelp)
+    {
+        this.jmHelp = jmHelp;
+    }
+
+    public JMenuBar getJmbMenuBar() {
+        return jmbMenuBar;
+    }
+
+    public void setJmbMenuBar(JMenuBar jmbMenuBar)
+    {
+        this.jmbMenuBar = jmbMenuBar;
+    }
+
+    public JMenuItem getJmiAbout()
+    {
+        return jmiAbout;
+    }
+
+    public void setJmiAbout(JMenuItem jmiAbout)
+    {
+        this.jmiAbout = jmiAbout;
+    }
+
+    public JMenuItem getJmiExit() {
+        return jmiExit;
+    }
+
+    public void setJmiExit(JMenuItem jmiExit) {
+        this.jmiExit = jmiExit;
+    }
+
+    public JMenuItem getJmiLogOut()
+    {
+        return jmiLogOut;
+    }
+
+    public void setJmiLogOut(JMenuItem jmiLogOut)
+    {
+        this.jmiLogOut = jmiLogOut;
+    }
+
+    public JLabel getUser()
+    {
+        return user;
+    }
+
+    public void setUser(JLabel user)
+    {
+        this.user = user;
+    }
+
+    public JLabel getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(JLabel userName)
+    {
+        this.userName = userName;
+    }
+
+}
